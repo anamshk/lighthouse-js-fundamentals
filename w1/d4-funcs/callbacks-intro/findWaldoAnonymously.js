391810e018cf6435d47e882a54d7696f959a363a
Anam Shaikh <anam.shk@outlook.com>
2021-10-28 18:35:35 -0400

    commit master

---
+++ w1/d4-funcs/callbacks-intro/findWaldoAnonymously.js
@@ -0,0 +1,18 @@
+// The second argument/parameter is expected to be a (callback) function
+const findWaldo = function(names, found) {
+  names.forEach(function(element, index) {
+    if (element === 'Waldo') {
+      found(index);
+    }
+  });
+};
+
+//const actionWhenFound = function(index) {
+//  console.log("Waldo is located at:" ${index}!`);
+//};
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index) {
+  console.log("Waldo is located at:", index);
+});
+
+//Found Waldo at index 2!
