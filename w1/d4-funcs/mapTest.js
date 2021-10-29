bf13b95417412d0344cfbe6f190690a5a72f5ad9
Anam Shaikh <anam.shk@outlook.com>
2021-10-28 21:59:22 -0400

    finished

---
+++ w1/d4-funcs/mapTest.js
@@ -0,0 +1,4 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+const map1 = lighthouses.map(i =>(i.length));
+console.log(map1);
+                    
